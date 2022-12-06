3ce51659c0be905f18732f8106e9506e5d7457f1
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-12-06 16:03:53 +0900

    ユーザ登録ページ作成

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,87 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(const SignInPage());
+}
+
+class SignInPage extends StatelessWidget {
+  const SignInPage();
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Card(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'Instagram',
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 12.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(hintText: 'Username'),
+                      validator: (value) => value!.trim().isEmpty
+                          ? 'Please enter a valid username.'
+                          : null,
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(hintText: 'Email'),
+                      validator: (value) => value!.contains('@')
+                          ? null
+                          : 'Please enter a valid email.',
+                    ),
+                    const SizedBox(
+                      height: 28.0,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 1.0,
+                          textStyle: TextStyle(
+                            color: Colors.white,
+                            backgroundColor: Theme.of(context).primaryColor,
+                          )),
+                      child: const Text('Sign Up'),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 12.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
